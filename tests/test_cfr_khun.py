f8c6ce88a8579092c45952dacec86a623beb890e
mmathiasT <tyminskimat@gmail.com>
2026-08-13 17:37:06 +0200

    Implement CFR training for Kuhn Poker, validated against known equilibrium

---
+++ tests/test_cfr_khun.py
@@ -0,0 +1,18 @@
+from train import training
+from games.kuhn_poker import Move, Card
+
+def strategy() -> dict[tuple, dict[Move, float]]:
+    return training(10000)
+
+
+def test_kings_call():
+    result_strategy = strategy()
+    assert result_strategy[(Card.K, (Move.BET, ))][Move.CALL] > 0.95
+
+def test_jack_dont_call():
+    result_strategy = strategy()
+    assert result_strategy[(Card.J, (Move.BET, ))][Move.CALL] < 0.05
+
+def test_king_bets_three_times_more_than_jack_bluffs():
+    result_strategy = strategy()
+    assert abs(3 * result_strategy[(Card.J, ())][Move.BET] - result_strategy[(Card.K, ())][Move.BET]) < 0.05
---
+++ train.py
@@ -0,0 +1,39 @@
+from games.kuhn_poker import Card, GameState, Move
+from cfr.cfr import CFRTrainer
+
+def possible_hands() -> list[list[Card]]:
+    result: list[list[Card]] = []
+
+    for card_p1 in Card:
+        for card_p2 in Card:
+            if card_p1 == card_p2:
+                continue
+            else:
+                result.append([card_p1, card_p2])
+
+    return result
+
+
+def training(iterations: int) -> dict[tuple, dict[Move, float]]:
+    trainer = CFRTrainer()
+    hands = possible_hands()
+
+    for iteration in range(0,iterations):
+        for hand in hands:
+            trainer.cfr(GameState(hand, []), [1, 1])
+
+    strategy_sum = trainer.strategy_sum 
+    final_strategy: dict[tuple, dict[Move, float]] = {}
+
+    for information_set, moves in strategy_sum.items():
+        possible_moves_sum = sum(moves.values())
+        for move in moves:
+            if information_set not in final_strategy:
+                final_strategy[information_set] = {}
+            final_strategy[information_set][move] = strategy_sum[information_set][move] / possible_moves_sum
+
+    return final_strategy
+
+if __name__ == "__main__":
+    final_strategy = training(10000)
+    print(final_strategy)
